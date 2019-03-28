fa74d5093e82fb522480f841352c3a2059532016
Seba casa <seba-pinto@hotmail.com>
2019-03-28 11:31:15 -0300

    Main modificado

--- main.py
+++ main.py
@@ -7,29 +7,17 @@ np.random.seed(seed)
 
 # --------------------------------------------------- #
 
-n = 400
+n = 100
+
 f = 10
-fz = 0.85
+delta = 1.00 / f
+
+fz = 0.50
 
 mysys = Mysys(n = n, f = f, fraction_of_zeros = fz, id_topology = 0.0)
+mysys.set_delta(delta)
 
-mysys.evol2convergence(delta = 1.00/f, threshold = 0.0)
-
-import matplotlib.pyplot as plt
-
-array = mysys.adjacency_matrix * mysys.get_corr_matrix()
-
-plt.figure(1)
-hist, edges = np.histogram(mysys.get_corr_matrix(), bins = np.arange(-0.025, 1.075, 0.05))
-plt.plot([(edges[i] + edges[i+1])*0.5 for i in range(len(hist))], hist, '.-', markersize = 20)
-plt.grid('on')
-
-
-plt.figure(2)
-hist, edges = np.histogram(array, bins = np.arange(-0.025, 1.075, 0.05)) 
-plt.plot([(edges[i] + edges[i+1])*0.5 for i in range(len(hist))], hist, '.-', markersize = 20)
-plt.grid('on')
+mysys.evol2convergence()
 
 print np.max(mysys.fragments())
 
-plt.show()
